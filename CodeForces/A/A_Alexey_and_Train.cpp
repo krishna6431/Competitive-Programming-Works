634e0a5220cfe5c993903d9ddf70479403dccef1
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-03-19 15:40:21 +0530

    added A. Alexey and Train

---
+++ CodeForces/A/A_Alexey_and_Train.cpp
@@ -0,0 +1,92 @@
+// All Important Header Files
+#include <bits/stdc++.h>
+//ALL IMPORTANT MACROS
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define deb(x) cout << x << endl;
+#define display(start, end) for (int i = start; i < end; i++)
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define alla(a, n) a, a + n
+#define endl "\n"
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+//USING NAME SPACE
+using namespace std;
+//SOME TYPEDEF DECLARATION
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+// CODE WRITTEN BY KRISHNA
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> arrival(n + 1);
+    vector<int> dept(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arrival[i] >> dept[i];
+    }
+    // for (auto x : arrival)
+    //     cout << x << " ";
+    // cout << endl;
+    // for (auto x : dept)
+    //     cout << x << " ";
+    // cout << endl;
+    vector<int> delay(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> delay[i];
+    }
+    int ans = 0;
+    int final_ans;
+    for (int i = 1; i <= n; i++)
+    {
+        ans += (delay[i] + arrival[i]) - dept[i - 1];
+
+        final_ans = ans;
+        int temp = dept[i] - arrival[i];
+        double temp2 = (double)temp / 2;
+        // cout << temp << " " << temp2 << endl;
+        int stay = ceil(temp2);
+        if (ans + stay <= dept[i])
+        {
+            ans = dept[i];
+        }
+        else
+        {
+            ans += stay;
+        }
+    }
+    cout << final_ans << endl;
+}
+
+int main()
+{
+    fast;
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("../input.txt", "r", stdin);
+    //     freopen("../output.txt", "w", stdout);
+    // #endif
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
