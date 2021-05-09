543d33aa90e6f4a6644eee410150f45e9444e048
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-05-10 02:29:27 +0530

    added Nastia and Good Array Problem CF Round 720

--- test.cpp
+++ test.cpp
@@ -1,12 +1,24 @@
-Problem 3. Given a set of N intervals, find a maximum subset S’ of S such that no pair of intervals in S’ overlaps.
-Algorithm:
-Sort the intervals according to their end points.
-Now, traverse through all the intervals, if we get two overlapping intervals, then greedily choose the interval with lower end point, since choosing it will ensure that intervals further can be accommodated without any overlap.
-Apply the same procedure for all the intervals and print all the intervals which satisfy the above criteria.
- 
-Pseudo Code:
-Input- The list of intervals and number of intervals.
-maxSubset(intervals, n)
+#include <bits/stdc++.h>
+using namespace std;
+int main()
 {
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+        }
+    }
 }
-Complexity Analysis : Time Complexity : O(N log N)[where N is the number of sets of intervals] Space Complexity : O(1)[No extra space is required]
