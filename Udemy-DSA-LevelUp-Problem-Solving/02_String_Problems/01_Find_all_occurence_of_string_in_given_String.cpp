0a673ba4148ee0658708a278baf86192008fe68c
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-05-31 15:48:47 +0530

    added find all occurance of string

--- Udemy-DSA-LevelUp-Problem-Solving/02_String_Problems/01_Find_all_occurence_of_string_in_given_String.cpp
+++ Udemy-DSA-LevelUp-Problem-Solving/02_String_Problems/01_Find_all_occurence_of_string_in_given_String.cpp
@@ -5,15 +5,18 @@
 #define mp make_pair
 #define fr first
 #define sc second
+#define endl "\n"
 #define deb(x) cout << x << endl;
-#define display(start, end) for (int i = start; i < end; i++)
+#define display(start, end)           \
+    for (int i = start; i < end; i++) \
+        cout << i << " ";             \
+    cout << endl;
 #define MOD 1000000007
 #define len(x) x.size()
 #define min3(a, b, c) min(a, min(b, c))
 #define max3(a, b, c) max(a, max(b, c))
 #define all(v) v.begin(), v.end()
 #define alla(a, n) a, a + n
-#define endl "\n"
 #define fast                          \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
@@ -28,7 +31,7 @@ typedef vector<ll> vll;
 typedef vector<pll> vpll;
 typedef vector<vll> vvll;
 typedef vector<string> vs;
-// CODE WRITTEN BY KRISHNA_6431
+// code is written by mr_krishna
 
 vector<int> findAll(string big, string small)
 {
