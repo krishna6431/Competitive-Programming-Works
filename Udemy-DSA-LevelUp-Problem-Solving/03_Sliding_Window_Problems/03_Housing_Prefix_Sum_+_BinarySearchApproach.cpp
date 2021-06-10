730695f0077e58b2783e1e775f3ad2fe44b4fdd6
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-06-10 21:35:11 +0530

    added housing problem prefixsum_bsearch_approach

---
+++ Udemy-DSA-LevelUp-Problem-Solving/03_Sliding_Window_Problems/03_Housing_Prefix_Sum_+_BinarySearchApproach.cpp
@@ -0,0 +1,111 @@
+// All Important Header Files
+#include <bits/stdc++.h>
+//ALL IMPORTANT MACROS
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define endl "\n"
+#define deb(x) cout << x << endl;
+#define display(start, end)           \
+    for (int i = start; i < end; i++) \
+        cout << i << " ";             \
+    cout << endl;
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define alla(a, n) a, a + n
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+//USING NAME SPACE
+using namespace std;
+//SOME TYPEDEF DECLARATION
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+// code is written by mr_krishna
+
+//Problem Description
+/*
+Along one side of a road there is a sequence of vacant plots of land.Each Plot has a different area(non - zero).So, the areas from a sequence A[1], A[2]... A[N].
+You want to buy K acres of land to build a house.You want ot find all segments of contiguious plots(i.e.a subsection in the sequence) of whose sum is exactly K.
+*/
+
+/*
+Sample Input:
+Plots = {1, 3, 2, 1, 4, 1, 3, 2, 1, 1, 2}
+K = 8
+
+Sample Output:
+2 5
+4 6
+5 9
+*/
+
+/*
+Time Complexity:O(nlogn)
+Space Complexity:O(n)
+*/
+
+//function for finding plots of lands equal to
+//prefix sum approach
+vector<pair<int, int>> prefixSum_BinarySearch_Housing(vector<int> v, int k)
+{
+    vector<pair<int, int>> ans;
+    vector<int> prefix;
+    prefix.push_back(v[0]);
+    for (int i = 1; i < v.size(); i++)
+    {
+        prefix.push_back(v[i] + prefix[i - 1]);
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        int findPotentialElement = prefix[i] + k;
+        if (binary_search(prefix.begin() + i, prefix.end(), findPotentialElement))
+        {
+            int loc_j = lower_bound(prefix.begin() + i, prefix.end(), findPotentialElement) - prefix.begin();
+            ans.push_back({i + 1, loc_j});
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    fast;
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("./input.txt", "r", stdin);
+    //     freopen("./output.txt", "w", stdout);
+    // #endif
+    int t;
+    cin >> t;
+    int tc = 1;
+    while (t--)
+    {
+        cout << "Case #" << tc++ << ":" << endl;
+        int n, k;
+        cin >> n >> k;
+        vector<int> input;
+        int temp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> temp;
+            input.push_back(temp);
+        }
+        vector<pair<int, int>> ans = prefixSum_BinarySearch_Housing(input, k);
+        for (auto x : ans)
+        {
+            cout << x.first << " " << x.second << endl;
+        }
+    }
+    return 0;
+}
