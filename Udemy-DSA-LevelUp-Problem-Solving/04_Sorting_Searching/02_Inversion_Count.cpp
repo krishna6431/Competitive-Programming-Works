4df52435e6201d5fcebc83ff505452d54586dd9c
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-06-23 23:51:56 +0530

    addded inversion count

---
+++ Udemy-DSA-LevelUp-Problem-Solving/04_Sorting_Searching/02_Inversion_Count.cpp
@@ -0,0 +1,85 @@
+// All Important Header Files
+#include <bits/stdc++.h>
+//ALL IMPORTANT MACROS
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define endl "\n"
+#define deb(x) cout << x << endl;
+#define display(start, end)           \
+    for (int i = start; i < end; i++) \
+        cout << i << " ";             \
+    cout << endl;
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define alla(a, n) a, a + n
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+//USING NAME SPACE
+using namespace std;
+//SOME TYPEDEF DECLARATION
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+// code is written by mr_krishna
+
+//Problem Description
+/*
+Count Number of Inversions
+*/
+
+/*
+Sample Input:
+8 9 7 6 5 4
+
+Sample Output:
+4 5 6 7 8 9
+*/
+
+/*
+Time Complexity:O(nlogn)
+Space Complexity:O(n)
+*/
+
+
+
+int main()
+{
+    fast;
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("./input.txt", "r", stdin);
+    //     freopen("./output.txt", "w", stdout);
+    // #endif
+    int t;
+    cin >> t;
+    int tc = 1;
+    while (t--)
+    {
+        cout << "Case #" << tc++ << ":" << endl;
+        int n;
+        cin >> n;
+        vector<int> arr;
+        int temp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> temp;
+            arr.push_back(temp);
+        }
+        mergeSort(arr, 0, n - 1);
+        for (auto x : arr)
+            cout << x << " ";
+        cout << endl;
+    }
+    return 0;
+}
