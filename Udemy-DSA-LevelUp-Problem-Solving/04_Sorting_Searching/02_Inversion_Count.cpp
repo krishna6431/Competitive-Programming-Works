0ca0965352f6c2d6ea5cb446f689fca8e4405581
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-06-24 22:03:52 +0530

    added count of inversion pair

--- Udemy-DSA-LevelUp-Problem-Solving/04_Sorting_Searching/02_Inversion_Count.cpp
+++ Udemy-DSA-LevelUp-Problem-Solving/04_Sorting_Searching/02_Inversion_Count.cpp
@@ -35,23 +35,80 @@ typedef vector<string> vs;
 
 //Problem Description
 /*
-Count Number of Inversions
+Count Inversion Count
 */
 
 /*
 Sample Input:
-8 9 7 6 5 4
+10 5 2 0 7 6 4
 
 Sample Output:
-4 5 6 7 8 9
+13
 */
 
 /*
 Time Complexity:O(nlogn)
 Space Complexity:O(n)
 */
+//brute force solution is findind all pair (tc = o(n^2))
 
+//helper method
+int crossInversion(vector<int> &array, int s, int e)
+{
+    //something happening wrong with this code
+    int i = s;
+    int m = (s + e) / 2;
+    int j = m + 1;
+    vector<int> temp;
+    int count = 0;
+    while (i <= m and j <= e)
+    {
+        if (array[i] < array[j])
+        {
+            temp.push_back(array[i]);
+            i++;
+        }
+        else
+        {
+            count += m - i + 1;
+            temp.push_back(array[j]);
+            j++;
+        }
+    }
+    //this loop add remaining elements from first array
+    while (i <= m)
+    {
+        temp.push_back(array[i++]);
+    }
+    //this loop add remaining elements from second array
+    while (j <= e)
+    {
+        temp.push_back(array[j++]);
+    }
+    //copy back all elements to original array
+    int k = 0;
+    for (int idx = s; idx <= e; idx++)
+    {
+        array[idx] = temp[k++];
+    }
+    return count;
+}
 
+// inversion count recursive function
+int inversionCount(vector<int> &array, int s, int e)
+{
+    //base case
+    if (s >= e)
+    {
+        return 0;
+    }
+    //rec case
+    int mid = (s + e) / 2;
+    int c1 = inversionCount(array, s, mid);
+    int c2 = inversionCount(array, mid + 1, e);
+    int ci = crossInversion(array, s, e);
+    return c1 + c2 + ci;
+}
 
 int main()
 {
@@ -76,10 +133,7 @@ int main()
             cin >> temp;
             arr.push_back(temp);
         }
-        mergeSort(arr, 0, n - 1);
-        for (auto x : arr)
-            cout << x << " ";
-        cout << endl;
+        cout << inversionCount(arr, 0, n - 1) << endl;
     }
     return 0;
 }
