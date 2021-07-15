56fb119ffc8d91998260e27792842e41a19d7152
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-07-15 23:18:35 +0530

    added sudoku problem

---
+++ Udemy-DSA-LevelUp-Problem-Solving/06_Recursion_Problems/06_Sudoku.cpp
@@ -0,0 +1,96 @@
+// All Important Header Files
+#include <bits/stdc++.h>
+//ALL IMPORTANT MACROS
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define endl "\n"
+#define deb(x) cout << x << endl;
+#define display(start, end)           \
+    for (int i = start; i < end; i++) \
+        cout << i << " ";             \
+    cout << endl;
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define alla(a, n) a, a + n
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+//USING NAME SPACE
+using namespace std;
+//SOME TYPEDEF DECLARATION
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+// code is written by mr_krishna
+
+//Problem Description
+/*
+Given a Partially filled suduko Matrix of 9x9,
+Write a function to fill the matrix using sudoku rules
+*/
+
+/*
+Sample Input:
+2D Martix
+
+Sample Output:
+Solved 2D Matrix
+*/
+
+/*
+Time Complexity: O(n*n!)
+Space Complexity:
+*/
+
+void permute(string str, int l, int r)
+{
+    // base case
+    if (l == r)
+    {
+        cout << str << endl;
+    }
+    else
+    {
+        //permutation made
+        for (int i = l; i <= r; i++)
+        {
+            // swap case
+            swap(str[l], str[i]);
+            // recursion
+            permute(str, l + 1, r);
+            //backtrack
+            swap(str[l], str[i]);
+        }
+    }
+}
+
+int main()
+{
+    fast;
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("./input.txt", "r", stdin);
+    //     freopen("./output.txt", "w", stdout);
+    // #endif
+    int t;
+    cin >> t;
+    int tc = 1;
+    while (t--)
+    {
+        cout << "Case #" << tc++ << ":" << endl;
+        string input;
+        cin >> input;
+        permute(input, 0, input.size() - 1);
+    }
+    return 0;
+}
