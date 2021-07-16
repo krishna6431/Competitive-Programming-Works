f945f6ad507806685a430267970fe674a9e49640
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-07-16 23:10:04 +0530

    added sudoku problem and testcase

--- Udemy-DSA-LevelUp-Problem-Solving/06_Recursion_Problems/06_Sudoku.cpp
+++ Udemy-DSA-LevelUp-Problem-Solving/06_Recursion_Problems/06_Sudoku.cpp
@@ -48,30 +48,83 @@ Solved 2D Matrix
 */
 
 /*
-Time Complexity: O(n*n!)
+Time Complexity: O(9^(n*n))
 Space Complexity:
 */
 
-void permute(string str, int l, int r)
+bool isSafe(int mat[][9], int i, int j, int no, int n)
 {
-    // base case
-    if (l == r)
+    //check for row and col
+    for (int k = 0; k < n; k++)
     {
-        cout << str << endl;
-    }
-    else
-    {
-        //permutation made
-        for (int i = l; i <= r; i++)
+        if (mat[k][j] == no || mat[i][k] == no)
         {
-            // swap case
-            swap(str[l], str[i]);
-            // recursion
-            permute(str, l + 1, r);
-            //backtrack
-            swap(str[l], str[i]);
+            return false;
         }
     }
+
+    //check for subgrid
+    int sx = (i / 3) * 3;
+    int sy = (j / 3) * 3;
+    for (int x = sx; x < sx + 3; x++)
+    {
+        for (int y = sy; y < sy + 3; y++)
+        {
+            if (mat[x][y] == no)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool solveSudoku(int mat[][9], int i, int j, int n)
+{
+    //base case
+    if (i == n)
+    {
+        //print solution
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << mat[i][j] << " ";
+            }
+            cout << endl;
+        }
+        return true;
+    }
+
+    // recursive case
+    if (j == n)
+    {
+        return solveSudoku(mat, i + 1, 0, n);
+    }
+    // skip prefilled cell
+    if (mat[i][j] != 0)
+    {
+        return solveSudoku(mat, i, j + 1, n);
+    }
+
+    // cell to be filled
+    //try out all posibilities
+    for (int no = 1; no <= n; no++)
+    {
+        // whether it is safe to place the number
+        if (isSafe(mat, i, j, no, n))
+        {
+            mat[i][j] = no;
+            bool solveSubproblem = solveSudoku(mat, i, j + 1, n);
+            if (solveSubproblem == true)
+            {
+                return true;
+            }
+        }
+    }
+    //if no option works
+    mat[i][j] = 0;
+    return false;
 }
 
 int main()
@@ -88,9 +141,16 @@ int main()
     while (t--)
     {
         cout << "Case #" << tc++ << ":" << endl;
-        string input;
-        cin >> input;
-        permute(input, 0, input.size() - 1);
+        int n = 9;
+        int mat[9][9];
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                cin >> mat[i][j];
+            }
+        }
+        solveSudoku(mat, 0, 0, n);
     }
     return 0;
 }
