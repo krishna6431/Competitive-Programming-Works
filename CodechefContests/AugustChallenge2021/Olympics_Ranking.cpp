26120a1e90a8d2ae7c7be871f98c7f49a4eb7bb9
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-08-15 04:15:19 +0530

    added august challenge 2021

---
+++ CodechefContests/AugustChallenge2021/Olympics_Ranking.cpp
@@ -0,0 +1,64 @@
+// All Important Header Files
+#include <bits/stdc++.h>
+//ALL IMPORTANT MACROS
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define deb(x) cout << x << endl;
+#define display(start, end) for (int i = start; i < end; i++)
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define alla(a, n) a, a + n
+#define endl "\n"
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+//USING NAME SPACE
+using namespace std;
+//SOME TYPEDEF DECLARATION
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+// CODE WRITTEN BY KRISHNA_6431
+
+void solve()
+{
+    vector<int> arr(6);
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int s1 = arr[0] + arr[1] + arr[2];
+    int s2 = arr[3] + arr[4] + arr[5];
+    s1 > s2 ? cout << "1" << endl : cout << "2" << endl;
+}
+
+int main()
+{
+    fast;
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    int t;
+    cin >> t;
+    // int tc = 1;
+    while (t--)
+    {
+        // cout << "Case #" << tc << ": ";
+        // tc++;
+        solve();
+    }
+    return 0;
+}
