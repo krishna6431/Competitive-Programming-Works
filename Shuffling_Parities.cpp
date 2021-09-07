027663f02b584575b85d1ad0ce8df5338a891182
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-09-08 01:29:13 +0530

    added codchef sepetember long challenge

---
+++ Shuffling_Parities.cpp
@@ -0,0 +1,74 @@
+// All Important Header Files
+#include <bits/stdc++.h>
+//ALL IMPORTANT MACROS
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define deb(x) cout << x << endl;
+#define display(start, end) for (int i = start; i < end; i++)
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define alla(a, n) a, a + n
+#define endl "\n"
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+//USING NAME SPACE
+using namespace std;
+//SOME TYPEDEF DECLARATION
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+// CODE WRITTEN BY KRISHNA_6431
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    ll temp;
+    ll odd_pos = n & 1 ? (n / 2) + 1 : n / 2;
+    ll even_pos = n - odd_pos;
+    ll even_cnt = 0;
+    ll odd_cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        if (temp & 1)
+        {
+            odd_cnt++;
+        }
+    }
+    even_cnt = n - odd_cnt;
+    ans += min(odd_cnt, even_pos) + min(even_cnt, odd_pos);
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast;
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    int t;
+    cin >> t;
+    // int tc = 1;
+    while (t--)
+    {
+        // cout << "Case #" << tc << ": ";
+        // tc++;
+        solve();
+    }
+    return 0;
+}
