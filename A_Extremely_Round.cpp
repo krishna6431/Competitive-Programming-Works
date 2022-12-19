bb2a872f95a40b690fe79a19d20855a23c34239a
krishna6431 <vermakrish921@gmail.com>
2022-12-20 00:36:43 +0530

    added some more problems

---
+++ A_Extremely_Round.cpp
@@ -0,0 +1,99 @@
+/*
+     ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् |
+     उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय माऽमृतात् ||
+*/
+// All Important Header Files
+#include <bits/stdc++.h>
+// ALL IMPORTANT MACROS
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define deb(x) cout << x << endl;
+#define display(start, end) for (int i = start; i < end; i++)
+#define MOD 1000000007
+#define len(x) x.size()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define all(v) v.begin(), v.end()
+#define alla(a, n) a, a + n
+#define endl "\n"
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+// USING NAME SPACE
+using namespace std;
+// SOME TYPEDEF DECLARATION
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+#define que_max priority_queue<int>
+#define que_min priority_queue<int, vi, greater<int>>
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1)
+{
+    cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
+}
+
+// CODE WRITTEN BY mr_krishna/krishna_6431
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = 9;
+    ll div = 10;
+    if (n < 9)
+    {
+        cout << n << endl;
+        return;
+    }
+    while (n / div)
+    {
+        ll temp = (n) / (div * 10);
+        if (temp == 0)
+        {
+            ans += n / div;
+        }
+        else
+        {
+            ans += 9;
+        }
+
+        div *= 10;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast;
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    int t;
+    cin >> t;
+    int tc = 1;
+    while (t--)
+    {
+        // cout << "Case #" << tc << ": ";
+        tc++;
+        solve();
+    }
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -3,40 +3,5 @@ using namespace std;
 
 int main()
 {
-    // your code goes here
-    int testcase;
-    cin >> testcase;
-    while (testcase--)
-    {
-        int n;
-        cin >> n;
-        string str;
-        cin >> str;
-        unordered_map<string, int> mp;
-        for (int i = 0; i < str.length(); i++)
-        {
-            string subStr;
-            // Second loop is generating sub-string
-            for (int j = i; j < str.length(); j++)
-            {
-                subStr += str[j];
-                mp[subStr]++;
-            }
-        }
-        vector<pair<int, string>> v;
-        for (auto x : mp)
-        {
-            if (x.second > 1)
-            {
-                v.push_back({(x.first).length(), x.first});
-            }
-        }
-        // for(auto x:v){
-        //     cout<<x.first<<" "<<x.second<<endl;
-        // }
-        sort(v.begin(), v.end());
-        cout << v.end() - v.begin() - 1 << endl;
-        // cout << v[v.end() - v.begin() - 1] << endl;
-    }
-    return 0;
+    cout << (2 | 3 | 4 | 7);
 }
